8e6ebc2481d7a6d190e26df8d696c22a4b0dd384
IS Choi <insil321@hotmail.com>
2018-10-03 00:23:28 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,12 +1,12 @@
 def my_hash
-  your_hash = {"sunflowers" => "yellow"; "daisy" => "blue"; "lilies" => "purple"}
+  your_hash = {"sunflowers" => "yellow", "daisy" => "blue", "lilies" => "purple"}
 
   
 end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corsets" => 5; "porcelain vases" => 2; "oil paintings" => 3}
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
@@ -28,8 +28,8 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-shipping_manifest["muskets"] => 2 
-shipping_manifest["gun poweder"] => 4
+shipping_manifest["muskets"] =>2 
+shipping_manifest["gun poweder"] =>4
 
   # add 4 gun powder to the shipping_manifest hash below
 
